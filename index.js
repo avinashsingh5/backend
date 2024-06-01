3a9d0252bcf8c5be120bec13baff8dd03baf9c85
avinashsingh5 <avinashkumar90202@gmail.com>
2024-06-01 23:10:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.get("/home",(req,res) => {
+  res.send("this is the home page of avinash website")
+})
+
+app.get("/login",(req,res) =>{
+  res.send("please login with your credentials")
+})
